2d918e30db4a08d8331719d6a3d5e7573a7b0123
Chris Rowe <chris.rowe@sonoport.com>
2014-12-01 11:13:19 +0000

    DTO and mapper for audio descriptors [Issue #25]

---
+++ src/main/java/com/sonoport/freesound/response/mapping/AudioDescriptorsMapper.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 Sonoport (Asia) Pte Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sonoport.freesound.response.mapping;
+
+import java.util.HashSet;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.sonoport.freesound.response.AudioDescriptors;
+
+/**
+ * Transform the JSON representation of the available audio descriptors into an {@link AudioDescriptors} object.
+ */
+public class AudioDescriptorsMapper extends Mapper<JSONObject, AudioDescriptors> {
+
+	@Override
+	public AudioDescriptors map(final JSONObject source) {
+		final AudioDescriptors audioDescriptors = new AudioDescriptors();
+
+		final JSONObject fixedLengthDescriptors = extractFieldValue(source, "fixed-length", JSONObject.class);
+		final JSONArray fixedLengthOneDimensionalDescriptors =
+				extractFieldValue(fixedLengthDescriptors, "one-dimensional", JSONArray.class);
+		audioDescriptors.setFixedLengthOneDimensional(new HashSet<>(parseArray(fixedLengthOneDimensionalDescriptors)));
+
+		final JSONArray fixedLengthMultiDimensionalDescriptors =
+				extractFieldValue(fixedLengthDescriptors, "multi-dimensional", JSONArray.class);
+		audioDescriptors.setFixedLengthMultiDimensional(
+				new HashSet<>(parseArray(fixedLengthMultiDimensionalDescriptors)));
+
+		final JSONArray variableLengthDescriptors = extractFieldValue(source, "variable-length", JSONArray.class);
+		audioDescriptors.setVariableLength(new HashSet<>(parseArray(variableLengthDescriptors)));
+
+		return audioDescriptors;
+	}
+
+}
