20f411ba06a1dac81dc18f928312fd2f16a01876
Chris Rowe <chris.rowe@sonoport.com>
2014-12-08 14:39:05 +0000

    Added Describe Sound query, refactor of Upload Sound [Issue #10]

---
+++ src/main/java/com/sonoport/freesound/query/sound/DescribeSound.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2014 Sonoport (Asia) Pte Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.sonoport.freesound.query.sound;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+import com.sonoport.freesound.License;
+
+/**
+ * Query used to provide additional details relating to a previously uploaded sound. Sounds requiring these queries are
+ * listed as 'Pending Description' in the 'Pending Uploads' query.
+ */
+public class DescribeSound extends AbstractSoundUploadQuery<DescribeSound> {
+
+	/** Name of the parameter to pass the filename through as. */
+	protected static final String UPLOAD_FILENAME_PARAMETER_NAME = "upload_filename";
+
+	/** Path to API endpoint. */
+	private static final String PATH = "/sounds/describe/";
+
+	/** Name of the previously uploaded file. */
+	private final String uploadFilename;
+
+	/**
+	 * @param uploadFilename Name of the previously uploaded file
+	 * @param description Description to apply to sound
+	 * @param license License to apply to sound
+	 * @param tags Tags to associate with sound
+	 * @param oauthToken OAuth2 access token to present
+	 */
+	public DescribeSound(
+			final String uploadFilename,
+			final String description,
+			final License license,
+			final Set<String> tags,
+			final String oauthToken) {
+		super(PATH, oauthToken);
+
+		this.uploadFilename = uploadFilename;
+		this.setDescription(description);
+		this.setLicense(license);
+		this.setTags(tags);
+	}
+
+	@Override
+	public Map<String, String> getRouteParameters() {
+		return Collections.emptyMap();
+	}
+
+	@Override
+	public Map<String, Object> getQueryParameters() {
+		final Map<String, Object> queryParameters = super.getQueryParameters();
+
+		queryParameters.put(UPLOAD_FILENAME_PARAMETER_NAME, uploadFilename);
+
+		return queryParameters;
+	}
+
+}
